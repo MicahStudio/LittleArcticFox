b9c4848f86f36eeb7dd04ad3404f4ea7e01f0e2c
luacloud <luacloud@gmail.com>
2017-12-17 00:16:01 +0800

    增加部分仓储方法

---
+++ LittleArcticFox.EntityFrameworkCore/Repositories/IRepository.cs
@@ -0,0 +1,46 @@
+﻿using LittleArcticFox.EntityFrameworkCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleArcticFox.EntityFrameworkCore.Repositories
+{
+    /// <summary>
+    /// 仓储接口
+    /// </summary>
+    public interface IRepository
+    {
+    }
+    /// <summary>
+    /// 仓储
+    /// </summary>
+    /// <typeparam name="TEntity">实体</typeparam>
+    /// <typeparam name="TPKey">主键类型</typeparam>
+    public interface IRepository<TEntity, TPKey> : IRepository where TEntity : class, IEntity<TPKey>, IAuditedEntity<TPKey>
+    {
+        /// <summary>
+        /// 通过主键获取实体
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        TEntity Get(TPKey key);
+        /// <summary>
+        /// 通过主键获取实体
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        Task<TEntity> GetAsync(TPKey key);
+        /// <summary>
+        /// 增加一条记录
+        /// </summary>
+        /// <param name="entity"></param>
+        void Instert(TEntity entity);
+        /// <summary>
+        /// 增加一条记录
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task InstertAsync(TEntity entity);
+    }
+}
---
+++ LittleArcticFox.EntityFrameworkCore/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using LittleArcticFox.EntityFrameworkCore.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleArcticFox.EntityFrameworkCore.Repositories
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TPKey"></typeparam>
+    public class Repository<TEntity, TPKey> : IRepository<TEntity, TPKey> where TEntity : class, IEntity<TPKey>, IAuditedEntity<TPKey>
+    {
+        private readonly LAFDbContext _dbContext;
+        private DbSet<TEntity> Table => _dbContext.Set<TEntity>();
+        public Repository(LAFDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// 通过主键获取实体
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public TEntity Get(TPKey key) => Table.Find(key);
+
+        /// <summary>
+        /// 通过主键获取实体
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public Task<TEntity> GetAsync(TPKey key) => Table.FindAsync(key);
+        /// <summary>
+        /// 增加一条记录
+        /// </summary>
+        /// <param name="entity"></param>
+        public void Instert(TEntity entity) => Table.Add(entity);
+        /// <summary>
+        /// 增加一条记录
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public Task InstertAsync(TEntity entity) => Table.AddAsync(entity);
+    }
+}
